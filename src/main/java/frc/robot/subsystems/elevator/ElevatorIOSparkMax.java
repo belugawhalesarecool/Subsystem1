974287905b5bc555c0eaf7917542247ad53d5f96
raghavmani0129 <148149838+raghavmani0129@users.noreply.github.com>
2023-12-15 21:44:13 -0500

    add methods from subsystem IO

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOSparkMax.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems.elevator;
+gpackage frc.robot.subsystems.elevator;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
@@ -35,14 +35,93 @@ public class ElevatorIOSparkMax implements ElevatorIO{
         elevatorEncoder.setPosition(0);
       }
 
-        @Override
-        public void updateInputs(ElevatorIOInputs inputs) {
-            inputs.positionMeters = encoder.getPosition();
-            inputs.velocityMeters = encoder.getVelocity();
-            inputs.appliedVolts = elevatorMotor.getAppliedOutput() * elevatorMotor.getBusVoltage();
-            inputs.currentAmps = new double[] {elevatorMotor.getOutputCurrent()};
-            inputs.tempCelsius = new double[] {elevatorMotor.getMotorTemperature()};
+    @Override
+    public void setPIDConstants(double p, double i, double d, double ff) {
+        pidController.setP(p);
+        pidController.setI(i);
+        pidController.setD(d);
+        pidController.setFF(ff);
     }
+
+    @Override
+    public void updateInputs(ElevatorIOInputs inputs) {
+        inputs.positionMeters = encoder.getPosition();
+        inputs.velocityMeters = encoder.getVelocity();
+        inputs.appliedVolts = elevatorMotor.getAppliedOutput() * elevatorMotor.getBusVoltage();
+        inputs.currentAmps = new double[] {elevatorMotor.getOutputCurrent()};
+        inputs.tempCelsius = new double[] {elevatorMotor.getMotorTemperature()};
+    }
+
+       
+    @Override
+    public void setVoltage(double motorVolts) {
+        elevatorMotor.setVoltage(motorVolts);
+    }
+
+    
+    @Override
+    public double getDistance() {
+        return encoder.getPosition();
+    }
+
+    
+    @Override
+    public void goToSetpoint(double setpoint) {
+        this.setpoint = setpoint;
+        pidController.setReference(setpoint, CANSparkMax.ControlType.kPosition);
+    }
+
+    @Override
+    public void setBrake(boolean brake) {
+        elevatorMotor.setIdleMode(brake ? IdleMode.kBrake : IdleMode.kCoast);
+    }
+
+    @Override
+    public boolean atSetpoint() {
+        return Math.abs(encoder.getPosition() - setpoint) < ELEVATOR_PID_TOLERANCE;
+    }
+
+    @Override
+    public void setP(double p) {
+        pidController.setP(p);
+    }
+
+    @Override
+    public void setI(double i) {
+        pidController.setI(i);
+    }
+
+    @Override
+    public void setD(double d) {
+        pidController.setD(d);
+    }
+
+    @Override
+    public void setFF(double ff) {
+        pidController.setFF(ff);
+    }
+
+    @Override
+    public double getP() {
+        return pidController.getP();
+    }
+
+    @Override
+    public double getI() {
+        return pidController.getI();
+    }
+
+    @Override
+    public double getD() {
+        return pidController.getD();
+    }
+
+    @Override
+    public double getFF() {
+        return pidController.getFF();
+    }
+
+    
 
 
 }
